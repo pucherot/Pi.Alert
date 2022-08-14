12d2452219479ff6afe5afc8ccafffebb749330f
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-08-14 19:44:41 +0200

    Add Commit note to update check
    I'll do my best to add some information to the update package in the future

--- front/php/server/updatecheck.php
+++ front/php/server/updatecheck.php
@@ -4,6 +4,7 @@ foreach (glob("../../../db/setting_language*") as $filename) {
 }
 if (strlen($pia_lang_selected) == 0) {$pia_lang_selected = 'en_us';}
 require '../templates/language/'.$pia_lang_selected.'.php';
+
 ?>
 <h5 class="text-aqua" style="font-size: 16px;"><?php echo $pia_lang['Maintenance_Github_package_a'];?>
 <?php
@@ -25,4 +26,26 @@ $local_time = date("d.m.Y, H:i", $utc_ts);
 echo $local_time;
 ?>       
     <?php echo $pia_lang['Maintenance_Github_package_b'];?>
+<a href="#" data-toggle="modal" data-target="#modal-update">&nbsp;<i class="fa fa-fw fa-info-circle"></i></a>
 </h5>
+
+
+<div class="modal fade" id="modal-update">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Update notes from Github</h4>
+            </div>
+            <div class="modal-body">
+                <p><?php echo $data['0']['commit']['message'];?></p>
+            </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
+              </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
