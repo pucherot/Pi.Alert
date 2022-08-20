5eeb9bd7559be83e173c92a9d13e6a6ba83fca69
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-08-20 14:18:58 +0200

    Maintaining

--- front/php/server/updatecheck.php
+++ front/php/server/updatecheck.php
@@ -39,10 +39,10 @@ echo $local_time;
                 <h4 class="modal-title">Update notes from Github</h4>
             </div>
             <div class="modal-body" style="text-align: left;">
-                <pre style="border: none"><?php echo $data['0']['commit']['message'];?></pre>
+                <pre style="border: none"><?php echo $data['0']['commit']['message'];?><br><br></pre>
             </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
         </div>
         <!-- /.modal-content -->
