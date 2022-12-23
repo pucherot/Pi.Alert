54eef261634e003b45f43e6331ffdbe8e152d5cf
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-12-23 10:45:49 +0100

    Update pialert_update.sh

--- install/pialert_update.sh
+++ install/pialert_update.sh
@@ -178,7 +178,7 @@ SCAN_SUBNETS    = '--localnet'
 # SCAN_SUBNETS    = [ '192.168.1.0/24 --interface=eth1', '192.168.1.0/24 --interface=eth0' ]
 EOF
 fi
-  sudo chmod 664 "$PIALERT_HOME/config/pialert.conf"
+  sudo chmod 775 "$PIALERT_HOME/config/pialert.conf"
 }
 
 # ------------------------------------------------------------------------------
@@ -240,7 +240,7 @@ update_permissions() {
   chmod +x "$PIALERT_HOME/back/pialert-cli"                         2>&1 >> "$LOG"
   chmod +x "$PIALERT_HOME/back/pialert.py"                          2>&1 >> "$LOG"
   chmod +x "$PIALERT_HOME/back/update_vendors.sh"                   2>&1 >> "$LOG"
-  sudo chmod -R 664 "$PIALERT_HOME/config"                          2>&1 >> "$LOG"
+  sudo chmod -R 775 "$PIALERT_HOME/config"                          2>&1 >> "$LOG"
   print_msg "- Create Logfile Symlinks..."
   touch "$PIALERT_HOME/log/pialert.vendors.log"
   touch "$PIALERT_HOME/log/pialert.1.log"
