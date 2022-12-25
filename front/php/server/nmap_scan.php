99bcf9d1ba755eacc6db06269c10ed71353a32aa
leiweibau <105860611+leiweibau@users.noreply.github.com>
2022-12-25 01:31:56 +0100

    Security risk

--- front/php/server/nmap_scan.php
+++ front/php/server/nmap_scan.php
@@ -3,14 +3,15 @@
 $PIA_HOST_IP = $_REQUEST['scan'];
 $PIA_SCAN_MODE = $_REQUEST['mode'];
 
-if ($PIA_SCAN_MODE == 'fast') {
-    exec('nmap -F '.$PIA_HOST_IP, $output);
-} elseif ($PIA_SCAN_MODE == 'normal') {
-    exec('nmap '.$PIA_HOST_IP, $output);
-} elseif ($PIA_SCAN_MODE == 'detail') {
-    exec('nmap -A '.$PIA_HOST_IP, $output);
-}
-
+if (filter_var($PIA_HOST_IP, FILTER_VALIDATE_IP)) {
+	if ($PIA_SCAN_MODE == 'fast') {
+    		exec('nmap -F '.$PIA_HOST_IP, $output);
+	} elseif ($PIA_SCAN_MODE == 'normal') {
+	    exec('nmap '.$PIA_HOST_IP, $output);
+	} elseif ($PIA_SCAN_MODE == 'detail') {
+	    exec('nmap -A '.$PIA_HOST_IP, $output);
+	}
+} else {exit;}
 echo '<h4>Scan ('.$PIA_SCAN_MODE.') Results of: '.$PIA_HOST_IP.'</h4>';
 echo '<pre style="border: none;">'; 
 foreach($output as $line){
