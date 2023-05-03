c0eefdac723dbc0c8f63e5865f8c34316bc285a2
leiweibau <105860611+leiweibau@users.noreply.github.com>
2023-05-03 21:33:16 +0200

    CSS work 03

--- front/services.php
+++ front/services.php
@@ -215,7 +215,7 @@ function list_standalone_services() {
 				if ($for_httpcode == "0") {$codecolor = "bg-red";}
 				if ($func_latency[$x] == '99999999') {$loop_latency = 'offline';} else { $loop_latency = $func_latency[$x] . 's';}
 
-				echo '       <div class="item ' . $codecolor . '" title="' . $func_scans[$x] . ' / HTTP: ' . $for_httpcode . ' / Latency: ' . $loop_latency . '"></div>';
+				echo '       <div class="single_scan ' . $codecolor . '" title="' . $func_scans[$x] . ' / HTTP: ' . $for_httpcode . ' / Latency: ' . $loop_latency . '"></div>';
 
 			}
 
@@ -298,7 +298,7 @@ function get_service_from_unique_device($func_unique_device) {
 
 				if ($func_latency[$x] == '99999999') {$loop_latency = 'offline';} else { $loop_latency = $func_latency[$x] . 's';}
 
-				echo '       <div class="item ' . $codecolor . '" title="' . $func_scans[$x] . ' / HTTP: ' . $for_httpcode . ' / Latency: ' . $loop_latency . '"></div>';
+				echo '       <div class="single_scan ' . $codecolor . '" title="' . $func_scans[$x] . ' / HTTP: ' . $for_httpcode . ' / Latency: ' . $loop_latency . '"></div>';
 
 			}
 
@@ -315,16 +315,6 @@ function get_service_from_unique_device($func_unique_device) {
 <!-- Page ------------------------------------------------------------------ -->
 
 <link rel="stylesheet" href="lib/AdminLTE/plugins/iCheck/all.css">
-<style type="text/css">
-    .progress-segment {display: flex; margin-bottom: 5px; margin-top: 10px;}
-    .item {width: 100%; background-color: lightgray; margin-right: 2px; height: 12px;
-      &:first-child {border-top-left-radius: 3px; border-bottom-left-radius: 3px;}
-      &:last-child {border-top-right-radius: 3px; border-bottom-right-radius: 3px;}
-    }
-    .orange-common {background-color: #f04500 !important;}
-    .item:hover:after {position: absolute; display: flex; content: attr(title); left: 0px; top: 0px; padding: 5px; padding-top: 8px; background-color: #913225; font-size: 16px; color: white; width: 100%; height: 38px;}
-    .item:hover {background-color: #aaa !important;}
-</style>
 
 <div class="content-wrapper">
 
