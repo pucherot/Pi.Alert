106b33acaadbab9743d9beff14bd4f57dc571c50
leiweibau <105860611+leiweibau@users.noreply.github.com>
2023-11-25 16:46:36 +0100

    Journal improvements
    In some cases, the journal cannot write to the database because it is being used by another process. To ensure that the journal entries are not lost, a buffer has now been introduced which is used if an entry cannot be written. If the next journal entry is to be written, the buffer is extended in the event of an error or, if successful, the buffer is processed so that older entries appear in the journal afterwards.

--- front/php/server/journal.php
+++ front/php/server/journal.php
@@ -21,6 +21,14 @@ function calc_configfile_hash_top() {
 function pialert_logging($LogClass, $Trigger, $LogString, $Hash, $Additional_Info) {
 	global $db;
 
+	$journalFile = '../db/temp_journal.sql';
+
+	if (file_exists('../../../config/pialert.conf')) {
+		$journalFile = '../../../db/pialert_journal_buffer';
+	} else {
+		$journalFile = '../db/pialert_journal_buffer';
+	}
+
 	if ($Hash == 1) {
 		$filehash = calc_configfile_hash();
 		if ($filehash == "") {$filehash = calc_configfile_hash_top();}
@@ -28,7 +36,32 @@ function pialert_logging($LogClass, $Trigger, $LogString, $Hash, $Additional_Inf
 
 	$Journal_DateTime = date('Y-m-d H:i:s');
 
+	// Query Insert
 	$sql = 'INSERT INTO "pialert_journal" ("Journal_DateTime", "LogClass", "Trigger", "LogString", "Hash", "Additional_Info") VALUES("' . $Journal_DateTime . '", "' . $LogClass . '", "' . $Trigger . '", "' . $LogString . '", "' . $filehash . '", "' . $Additional_Info . '")';
-	$result = $db->query($sql);
+	$result = $db->exec($sql);
+
+	if ($result === false) {
+		if (file_exists($journalFile) && filesize($journalFile) > 0) {
+			// Append the error message to the file
+			file_put_contents($journalFile, PHP_EOL . $sql, FILE_APPEND);
+		} else {
+			// Create a file with the error message
+			file_put_contents($journalFile, $sql);
+		}
+	} else {
+		if (file_exists($journalFile) && filesize($journalFile) > 0) {
+			$queries = file($journalFile, FILE_IGNORE_NEW_LINES);
+			foreach ($queries as $query) {
+				$result = $db->exec($query);
+
+				// if ($result !== false) {
+				// 	echo "Query executed successfully. Rows affected: " . $result . PHP_EOL;
+				// } else {
+				// 	echo "Error executing query: " . $db->lastErrorMsg() . PHP_EOL;
+				// }
+			}
+			unlink($journalFile);
+		}
+	}
 }
 ?>
